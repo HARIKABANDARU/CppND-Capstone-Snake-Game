fb83d607f8e4fd19e8498188faf222a3d8150eb1
markusbuchholz <markus.x.buchholz@gmail.com>
2019-10-30 00:42:54 +0100

    add main test to test manager locally. to be removed

---
+++ src/main_test_player.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "GameManager.h"
+//#include "Player.h"
+
+
+
+int main(){
+
+
+
+GameManager gm1;
+//Player p2("markus", 10, "Mon Oct 28 21:43:13 2019");
+// Player p1;
+// p1.SetPlayerName();
+// p1.SetPlayerScore();
+// p1.SetPlayerGameTime();
+
+// std::cout<<"player ::  " << p1.GetPlayerName() << " :: " << p1.GetPlayerScore() << " :: " <<p1.GetPlayerGameTime() << std::endl;
+
+//string name = p2.GetPlayerGameTime();
+//std::cout<<"name :: "<< p2.GetPlayerName()<<std::endl;
+    return 0;
+
+
+};
